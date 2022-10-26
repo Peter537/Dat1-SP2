79235b8e1fc0fa2ee6ad16acbd4154c98c14430a
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-10-26 10:49:04 +0200

    Update FleetOfCars.java

--- Biludlejning/src/FleetOfCars.java
+++ Biludlejning/src/FleetOfCars.java
@@ -2,8 +2,22 @@ import java.util.ArrayList;
 
 public class FleetOfCars {
 
-    private ArrayList<Car> cars;
+    private final ArrayList<Car> cars = new ArrayList<>();
 
+    public FleetOfCars() { }
 
+    public void addCar(Car car) {
+        cars.add(car);
+    }
 
+    public int getTotalRegistrationFeeForFleet() {
+        return cars.stream().mapToInt(Car::getRegistrationFee).sum();
+    }
+
+    @Override
+    public String toString() {
+        return "FleetOfCars{" +
+                "cars=" + cars +
+                '}';
+    }
 }
