0e523342aa59a87797df3dee8161113a44f4b9d9
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-10-26 11:22:30 +0200

    update

--- Biludlejning/src/Main.java
+++ Biludlejning/src/Main.java
@@ -14,7 +14,7 @@ public class Main {
         fleetOfCars.addCar(new ElectricCar("MN 34 567", "Tesla", "Model S", 5, 100, 502));
         fleetOfCars.addCar(new ElectricCar("OP 56 789", "Tesla", "Model X", 5, 105, 543));
         fleetOfCars.addCar(new ElectricCar("QR 78 901", "BMW", "i3", 3, 38, 203));
-        
+
         System.out.println(fleetOfCars);
         System.out.println("Total registration fee for fleet: " + fleetOfCars.getTotalRegistrationFeeForFleet());
     }
